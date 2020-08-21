a7e586a8773e44e58669df1cfad5b4e57056a829
Garrett Hudson <garretthudson87@yahoo.com>
2020-08-20 21:36:46 -0400

    added more pseudocode

--- script.js
+++ script.js
@@ -6,3 +6,20 @@
 // - that data will be stored in global variables and used to plug into a function to predict the time of their 40
 // - that time will be used to compare to all of the pros (or a select number based on filters by position or year) and return a value (#/##);
 // - that prediction and comparison will be shown on the screen
+
+
+
+
+
+
+// predict formula using 10 yard and BW
+
+// if (athlete body weight is < 200lbs) {
+//   40 = 10yd split + 2.8-3.0
+// } else if (ath bw is >= 200 && ath bw is < 250 ) {
+//   40 = 10 yd + 3.0 - 3.2
+// } else if (ath bw is >= 250 && ath bw is <= 325) {
+//   40 = 10 yd + 3.2 - 3.4
+// } else {
+//   40 = 10 yd + 4
+// }
