a1754d75e43a32a9268ff0401c1d78ba98b55a09
Garrett Hudson <garretthudson87@yahoo.com>
2020-08-20 20:38:10 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// PSEUDO CODE
+
+// 1) landing page will have good branding, moving background, and buttons to send you to the predictor page, the results for nfl players page, the comparison page, or the video page
+
+// 2) the predictor page will take user input for their height, weight, and at least one of their 10 and 20 yard split.
+// - that data will be stored in global variables and used to plug into a function to predict the time of their 40
+// - that time will be used to compare to all of the pros (or a select number based on filters by position or year) and return a value (#/##);
+// - that prediction and comparison will be shown on the screen
